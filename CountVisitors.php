eaf90c3578485c0b5db006a2636bef8d57095808
Kiranmayee544 <141422565+Kiranmayee544@users.noreply.github.com>
2023-10-11 22:58:37 +0530

    Add files via upload

---
+++ CountVisitors.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if (isset($_SESSION['visitor_count'])) {
+    $_SESSION['visitor_count']++;
+} else {
+    $_SESSION['visitor_count'] = 1;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+.footer{
+	position:fixed;left:0;bottom:0;width:100%;color:white;background:green;text-align:center;
+}
+</style>
+<title>Page Visitor Count</title>
+</head>
+<body>
+    <h2 align="center">This is First PHP Program</h2>
+    
+<div class="footer">
+<p align="center"> &copy; A. Dhana Lakshmi -- Allrights Reserved
+<p>This page has been visited 
+<?php 
+	echo $_SESSION['visitor_count']; 
+?> 
+times.</p>
+</div>
+</body>
+</html>
